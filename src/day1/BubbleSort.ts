765e8094c83ce75022485736b02f1628cb51f738
jahanzebshayan <jahanzebshayan14@gmail.com>
2022-11-17 02:20:57 +0530

    BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
 
+    var tmp;
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+
+            }
+        }
+
+    }
+
 }
