519ff489e8d1468b747c1afe18e24f1b363d773a
jahanzebshayan <jahanzebshayan14@gmail.com>
2022-11-16 22:06:25 +0530

    search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    var lo = 0;
+    var hi = haystack.length + 1;
+
+    do{
+        var m = Math.floor(lo + (hi - lo)/2)
+        
+        if (haystack[m] === needle)
+            return true;
+        
+        else if (haystack[m] < needle)
+            lo = m+1;
+
+        else
+            hi = m;
+
+    }while(lo < hi)
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    var jmp = Math.floor(Math.sqrt(breaks.length));
+    var step = jmp;
+
+    for (; step < breaks.length; step += jmp) {
+        if (breaks[step] == true) {
+            break
+        }
+    }
+
+    step -= jmp;
+    for (let i = 0; i < jmp && step < breaks.length; i++, step++) {
+        if (breaks[step] == true) {
+            return step
+        }
+    }
+
+    return -1;
 }
