9ae7b7f48bc00ca6d5be2875638617a0d1229535
jahanzebshayan <jahanzebshayan14@gmail.com>
2022-11-20 02:48:03 +0530

    stack, queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = {value : item} as Node<T>
+        this.length++
+        if(!this.tail){
+            this.head = this.tail = node         
+        }
 
-}
+        this.tail.next = node
+        this.tail = node
+
+        console.log(this.tail.value)
+
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        head.next = undefined
+
+        if(this.length == 0){
+            this.tail = undefined
+        }
+        console.log(head, head.value)
+        return head.value
+
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {value : item} as Node<T>
+        node.next = undefined;
+        this.length++
+        if(!this.head){
+            this.head = node;
+            return
+        }
 
-}
+        node.next = this.head
+        this.head = node
+        console.log( node, this.head)
+    }
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
 
-}
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        console.log(head, head.value)
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
